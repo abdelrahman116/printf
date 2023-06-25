be1ebeae81d1e822eb40966b614b2cfc1494e943
abdelrahman <abdelrahman@github.com>
2023-06-25 16:21:41 -0700

    Printf

---
+++ prtscdf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+int _printf(const char * const format, ...)
+{
+	int i , n = strlen(format);
+	va_list printt;
+	va_start (printt,format);
+
+
+	for (i=0;i<n;i++)
+	{
+
+
+		if(((format[i] == '%') & (format[i+1] == 'd')) )
+		{
+
+				int x = va_arg (printt,int);
+					printf("%d",x);
+
+		}
+			else if(((format[i] == '%') & (format[i+1] == 's' )))
+			{
+
+					char *x = va_arg (printt,char *);
+					printf("%s",x);
+
+			}
+			else if(((format[i] == '%') & (format[i+1] == 'f' )))
+			{
+
+					double x = va_arg (printt,double);
+					printf("%f",x);
+
+			}
+			else if(((format[i] == '%') & (format[i+1] == 'c')) )
+			{
+
+					unsigned char x = (unsigned char) va_arg(printt, int);
+					printf("%c",x);
+
+			}
+			else
+			{
+
+				printf("%c",format[i]);
+			}
+
+	}
+	return (0);
+}
