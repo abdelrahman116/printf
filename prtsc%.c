fda0119c2c395b451090dac79dfec2da2c3c1315
abdelrahman <abdelrahman@github.com>
2023-06-26 12:45:46 -0700

    Printf

---
+++ prtsc%.c
@@ -0,0 +1,37 @@
+#include "main.h"
+int _printf(const char * const format, ...)
+{
+	int i, n = strlen(format);
+
+	va_list printt;
+
+	va_start(printt, format);
+	for (i = 0; i < n; i++)
+	{
+		    if (((format[i] == '%') & (format[i + 1] == 'c')))
+			{
+				unsigned char x = (unsigned char) va_arg(printt, int);
+
+						write(1,&x,sizeof(char));
+					i++;
+			}
+            else if (((format[i] == '%') & (format[i + 1] == 's')))
+			{
+				char *x = va_arg(printt, char *);
+
+					write(1 , &x , sizeof(char *));
+					i++;
+			}
+			else if (((format[i] == '%') & (format[i + 1] == '%')))
+			{
+					write(1 , &format[i], sizeof(char));
+                    i++;
+			}
+			else
+			{
+				write(1 , &format[i] , 1);
+			}
+	}
+	return (0);
+}
+
