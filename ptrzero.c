c2bcd19b6120d30e74b152cd13a55736f6f06b82
abdelrahman <abdelrahman@github.com>
2023-06-29 16:00:04 -0700

    Printf

--- ptrzero.c
+++ ptrzero.c
@@ -1,11 +1,11 @@
 #include "main.h" 
 
 int _printf(const char *format, ...)
-{
+{	
+	int count = 0;
     va_list args;
     va_start(args, format);
 
-    int count = 0;
 
     while (*format != '\0')
     {
