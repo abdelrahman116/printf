2cab25b95df3c8caa0da6b980f58f9c32a389625
abdelrahman <abdelrahman@github.com>
2023-06-30 17:50:07 -0700

    Printf

--- PTR_main.c
+++ PTR_main.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
 #include "main.h"
+
 void print_binary(unsigned int num)
 {
 	if (num > 1)
@@ -12,22 +10,21 @@ void print_binary(unsigned int num)
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
 	va_list args;
 	va_start(args, format);
+
+	int count = 0;
 
 	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
-			format++; /* Move past the '%' */
+			format++;
 
-			/* Handle the conversion specifiers */
 			switch (*format)
 			{
 				case 'c':
 				{
-					/* Fetch the next argument of type 'int' and print it as a character */
 					int ch = va_arg(args, int);
 					putchar(ch);
 					count++;
@@ -36,7 +33,7 @@ int _printf(const char *format, ...)
 
 				case 's':
 				{
-					/* Fetch the next argument of type 'char*' and print it as a string */
+
 					const char *str = va_arg(args, const char *);
 					count += fputs(str, stdout);
 					break;
@@ -45,7 +42,6 @@ int _printf(const char *format, ...)
 				case 'd':
 				case 'i':
 				{
-					/* Fetch the next argument of type 'int' and print it as a decimal integer */
 					int num = va_arg(args, int);
 					count += printf("%d", num);
 					break;
@@ -53,29 +49,23 @@ int _printf(const char *format, ...)
 
 				case 'b':
 				{
-					/* Fetch the next argument of type 'unsigned int' and print it as binary */
 					unsigned int num = va_arg(args, unsigned int);
 					print_binary(num);
-					count += sizeof(num) * 8; /* Assuming 32-bit unsigned int */
+					count += sizeof(num) * 8;
 					break;
 					}
 
 				case '%':
 				{
-					/* Print a literal '%' character */
 					putchar('%');
 					count++;
 					break;
 				}
 
-				default:
-					/* Unsupported conversion specifier, ignore it */
-					break;
 			}
 		}
 		else
 		{
-			/* Regular character, print it */
 			putchar(*format);
 			count++;
 		}
@@ -87,3 +77,4 @@ int _printf(const char *format, ...)
 
 	return count;
 }
+
